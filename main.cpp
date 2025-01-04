e35138320b8e3edaeb0d47dbee999a7bab0a012e
huanshang141 <1375436987@qq.com>
2025-01-04 14:43:41 +0800

    c2-triangle

--- main.cpp
+++ main.cpp
@@ -9,18 +9,19 @@ const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 const char *vertexShaderSource = "#version 330 core\n"
                                  "layout (location = 0) in vec3 aPos;\n"
-//                                 "out vec4 vertexColor;\n"
+                                 "out vec4 vertexColor;\n"
                                  "void main()\n"
                                  "{\n"
                                  "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-//                                 "   vertexColor = vec4(0.5,0.0,0.0,1.0);\n"
+                                 "   vertexColor = vec4(0.5,0.0,0.0,1.0);\n"
                                  "}\0";
 const char *fragmentShaderSource = "#version 330 core\n"
                                    "out vec4 FragColor;\n"
-                                   "uniform vec4 ourColor;\n"
+                                   "in vec4 vertexColor;\n"
                                    "void main()\n"
                                    "{\n"
-                                   "   FragColor = ourColor;\n"
+                                   "   FragColor = vertexColor;\n"
+//                                   "   FragColor = ourColor;\n"
                                    "}\n\0";
 int main()
 {
